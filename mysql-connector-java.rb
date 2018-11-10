27653acaee4284a04520265e0bc97989a88eee42
friverao <44882495+friverao@users.noreply.github.com>
2018-11-10 13:06:34 +0000

    Updating sha256 for MySQL Connector for Java
    Version: Connector/J 8.0.13

--- mysql-connector-java.rb
+++ mysql-connector-java.rb
@@ -4,7 +4,7 @@ class MysqlConnectorJava < Formula
   homepage "http://dev.mysql.com/downloads/connector/j/"
   version "8.0.13"
   url "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-#{version}.tar.gz"
-  sha256 "4d261f669f685cd9e1eb5683d03019466cb9e59a8b873efce1b623254ea29231"
+  sha256 "99501fbc74b4cb80cd75a4d06c38b662be01bfd39c409efa3c747ec83216329b"
 
   def install
     prefix.install_metafiles
